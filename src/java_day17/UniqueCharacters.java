7814b968b7ae9a85c35cf1755bd1983d9cb137bf
Jen Nest <154011445+jenneft@users.noreply.github.com>
2024-02-21 19:57:36 -0800

    updated 2/21/24

--- src/java_day17/UniqueCharacters.java
+++ src/java_day17/UniqueCharacters.java
@@ -1,6 +1,25 @@
 package java_day17;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class UniqueCharacters {
+    public static void main(String[] args) {
+        String string = "aabcccdeeeef";
+        Map<Character,Integer> map = uniqueCharacters(string);
+        System.out.println("map = " + map);
+    }
+
+    private static Map<Character, Integer> uniqueCharacters(String string) {
+        Map<Character,Integer> uniqueChars=new LinkedHashMap<>();
+        for (char c:string.toCharArray()){
+            uniqueChars.put(c,uniqueChars.getOrDefault(c,0)+1);
+        }
+        uniqueChars.entrySet().removeIf(p->p.getValue() !=1);
+        return uniqueChars;
+    }
+
+
 }
 /*
 5. Create a class named UniqueCharacters and write a program that returns the unique characters from a given
